374ce9b1c642ab2e983d40207d3384ff37984849
Nathan Tran <42622945+Ntran192@users.noreply.github.com>
2025-01-06 13:02:16 -0800

    Added "Hello World" Project

---
+++ Project1/Project1.cpp
@@ -0,0 +1,10 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream> /* cin, cout */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
